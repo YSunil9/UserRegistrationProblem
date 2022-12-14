d0c0b4259e35351f6f7234e1eee39ae763c90da5
Ysunil9 <sunil.pcs24@gmail.com>
2022-12-14 23:16:45 +0530

    Welcome

--- RegexUserRegistration/RegexClass.cs
+++ RegexUserRegistration/RegexClass.cs
@@ -47,7 +47,7 @@ namespace RegexUserRegistration
         }
         public void Password(string password)
         {
-            const string passwordRegex = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{8,}$";
+            const string passwordRegex = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[#!@$%*-])(?=.*[0-9])).{8,}$";
             if (Regex.IsMatch(password, passwordRegex))
             {
                 Console.WriteLine(password + " is a valid Password");
